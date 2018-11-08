5ed4bef32255b34879f69c37b3160898f11b38cd
John Mark Redding <johnmarkredding@gmail.com>
2018-11-08 19:34:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  prime = true
+  count = num / 2
+  if num != 1
+    while (count != 1 || prime != false)
+      if num % count == 0
+        prime = false
+      end
+      count -= 1
+    end
+  else
+    prime = false
+  end
+  prime
+end
