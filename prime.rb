6b5da31b9b7e23fcbb5c07d38ec26264a435d7e5
John Mark Redding <johnmarkredding@gmail.com>
2018-11-08 19:45:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(num)
   elsif num == 2
     prime = true
   else
-    while (count <= (num / 2) && prime != false)
+    while (count <= (num / 2) && prime == true)
       if num % count == 0
         prime = false
       end
